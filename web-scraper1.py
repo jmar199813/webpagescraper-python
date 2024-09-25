7b8266961a7b5f7fa1a05629f2c740038d985451
jmar <jmar199813@gmail.com>
2024-09-24 21:52:20 -0600

    Learned about website scraping. This udpates the code so that I don't accidently scarpe something i'm not suppsoed to.
    I assume a one off accidental scrape is allowed and they won't care.
    
    It seems to for data farmers with copyrighted material.
    
    I shouldn't run into that issue anyways anymore
    
    All in all this was a good mini demo on web scraping.
    
    I'm sure I can use it for a lot of other things such as monitoring a blog forum for spam/malicious content

--- web-scraper1.py
+++ web-scraper1.py
@@ -1,7 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.robotparser import RobotFileParser
 
-url = 'https://en.wikipedia.org/wiki/Main_Page'  # Replace with the target URL
+# Function to check if scraping is allowed
+def is_scraping_allowed(url):
+    # Parse the base URL to get the robots.txt URL
+    base_url = '/'.join(url.split('/')[:3])  # Extract protocol and domain
+    robots_url = f"{base_url}/robots.txt"
+    
+    rp = RobotFileParser()
+    rp.set_url(robots_url)
+    rp.read()
+    
+    return rp.can_fetch("*", url)
+
+url = input("Enter the URL to scrape: ")
+
+if not is_scraping_allowed(url):
+    print("Scraping not allowed by robots.txt")
+    exit()
 
 try:
     response = requests.get(url)

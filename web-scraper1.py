98e170a3780702e85c35137e315188dd86a246b0
jmar <jmar199813@gmail.com>
2024-09-24 21:10:14 -0600

    Found this template but it doesn't look like its going ot do much. First off, changing the url can only be done in the code. Maybe thats all it does, not sure. I just want to see if anything works

--- web-scraper1.py
+++ web-scraper1.py
@@ -1,2 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = 'https://example.com'  # Replace with the target URL
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an error for bad responses
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    headings = soup.find_all(['h1', 'h2', 'h3'])
+    for heading in headings:
+        print(heading.text.strip())
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
